670b849dbd8fe6615caea3398445d8d4318d5780
DKS-AR <dheeru.k.singh@gmail.com>
2020-01-24 12:01:16 -0600

    wip: fix the review comment

--- fetch/rest.ts
+++ fetch/rest.ts
@@ -2,5 +2,4 @@ const request = require('request');
 export const req = request('http://www.google.com', function (error:any, response:any, body:any) {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
 });
